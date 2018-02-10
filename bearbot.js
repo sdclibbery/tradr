94d42343519dce07ab3c4f4b6be339e8d6832ef1
Stephen Clibbery <sdclibbery@googlemail.com>
2018-02-10 10:03:32 +0000

    bull and bear simulation bots

---
+++ bearbot.js
@@ -0,0 +1,42 @@
+const Gdax = require('gdax');
+
+const websocket = new Gdax.WebsocketClient(['BTC-EUR']);
+
+let trade = undefined
+
+websocket.on('message', data => {
+  const {type, side, reason, price, time} = data
+  if (type === 'match') {
+    console.log(`match: ${price} ${side}`)
+    if (!trade) {
+      trade = {
+        sell_in: price,
+        sell_in_time: time,
+        stop_loss: price*1.01
+      }
+      console.log(`* starting trade; stop loss: ${trade.stop_loss}`)
+    } else {
+      const new_stop_loss = price*1.01
+      if (new_stop_loss < trade.stop_loss) {
+        console.log(`* moving stop loss to: ${trade.stop_loss}`)
+        trade.stop_loss = new_stop_loss
+      }
+      if (price >= trade.stop_loss) {
+        trade.buy_out = price
+        trade.buy_out_time = time
+        const profit = trade.sell_in - trade.buy_out
+        console.log(`* trade complete: profit ${profit} ${100*profit/trade.sell_in}%`)
+        console.log(trade)
+        process.exit()
+      }
+    }
+  }
+});
+
+websocket.on('error', err => {
+  console.log('error: ', err)
+});
+
+websocket.on('close', () => {
+  console.log('close')
+});
---
+++ bullbot.js
@@ -0,0 +1,42 @@
+const Gdax = require('gdax');
+
+const websocket = new Gdax.WebsocketClient(['BTC-EUR']);
+
+let trade = undefined
+
+websocket.on('message', data => {
+  const {type, side, reason, price, time} = data
+  if (type === 'match') {
+    console.log(`match: ${price} ${side}`)
+    if (!trade) {
+      trade = {
+        buy_in: price,
+        buy_in_time: time,
+        stop_loss: price*0.99
+      }
+      console.log(`* starting trade; stop loss: ${trade.stop_loss}`)
+    } else {
+      const new_stop_loss = price*0.99
+      if (new_stop_loss > trade.stop_loss) {
+        console.log(`* moving stop loss to: ${trade.stop_loss}`)
+        trade.stop_loss = new_stop_loss
+      }
+      if (price <= trade.stop_loss) {
+        trade.sell_out = price
+        trade.sell_out_time = time
+        const profit = trade.sell_out - trade.buy_in
+        console.log(`* trade complete: profit ${profit} ${100*profit/trade.buy_in}%`)
+        console.log(trade)
+        process.exit()
+      }
+    }
+  }
+});
+
+websocket.on('error', err => {
+  console.log('error: ', err)
+});
+
+websocket.on('close', () => {
+  console.log('close')
+});
