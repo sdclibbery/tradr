3398d6df3527ca38f225f681337c80b72a610c63
Stephen Clibbery <sdclibbery@googlemail.com>
2018-02-14 13:38:04 +0000

    rename state functions for clarity

--- trade.js
+++ trade.js
@@ -5,19 +5,18 @@ exports.trade = (options) => {
   let stoploss
   let exitPrice
   let exitTime
+  let state
 
-  const calcStoploss = (price) => price*(1 - percent/100)
-  const dp2 = (x) => Number.parseFloat(x).toFixed(2)
-
-  const initial = (price, time) => {
+  const stateBegin = (price, time) => {
     entryPrice = price
     entryTime = time
     enterTheMarket(entryPrice)
     setStoploss(entryPrice)
     return `${time} ${options.type} ${dp2(percent)}% starting ${options.amount} ${options.product} trade from ${dp2(entryPrice)}; initial stop loss: ${dp2(stoploss)}`
   }
+  state = stateBegin
 
-  const running = (price, time) => {
+  const stateRunning = (price, time) => {
     const bearShouldMoveStoploss = options.type === 'bear' && calcStoploss(price) < stoploss
     const bullShouldMoveStoploss = options.type === 'bull' && calcStoploss(price) > stoploss
     if (bearShouldMoveStoploss || bullShouldMoveStoploss) {
@@ -31,17 +30,20 @@ exports.trade = (options) => {
     if (bearComplete || bullComplete) {
       exitPrice = price
       exitTime = time
-      state = done
+      state = stateDone
       const profitPercent = (options.type === 'bull' ? 1 : -1) * 100 * (exitPrice - entryPrice) / entryPrice
       return `${time} ${options.type} ${dp2(percent)}% trade complete: ${dp2(entryPrice)}->${dp2(exitPrice)} profit ${dp2(profitPercent)}% ${entryTime}-${exitTime}`
     }
   }
 
-  const done = (price, time) => {}
-  let state = initial
+  const stateDone = (price, time) => {}
+
+  const calcStoploss = (price) => price*(1 - percent/100)
+
+  const dp2 = (x) => Number.parseFloat(x).toFixed(2)
 
   const enterTheMarket = (price) => {
-    state = running
+    state = stateRunning
   }
 
   const clearStoploss = () => {
@@ -54,6 +56,6 @@ exports.trade = (options) => {
   const newTrade = (price, time) => {
     return state(price, time.substring(11, 19))
   }
-  newTrade.done = () => state === done
+  newTrade.done = () => state === stateDone
   return newTrade
 }
