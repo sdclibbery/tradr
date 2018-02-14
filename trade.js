b652147b9e9fb29be6368acdcc4e58674d2ac568
Stephen Clibbery <sdclibbery@googlemail.com>
2018-02-14 11:59:09 +0000

    better log formatting

--- trade.js
+++ trade.js
@@ -7,14 +7,14 @@ exports.trade = (options) => {
   let exit_time
 
   const calcStoploss = (price) => price*(1 - percent/100)
-  const dp2 = (s) => s.toFixed(2)
+  const dp2 = (x) => Number.parseFloat(x).toFixed(2)
 
   const initial = (price, time) => {
     entry = price
     entry_time = time
     stoploss = calcStoploss(price)
     state = running
-    return `* ${options.type} ${dp2(percent)}% starting trade at ${time} from ${dp2(entry)}; initial stop loss: ${dp2(stoploss)}`
+    return `${time} ${options.type} ${dp2(percent)}% starting trade from ${dp2(entry)}; initial stop loss: ${dp2(stoploss)}`
   }
 
   const running = (price, time) => {
@@ -22,7 +22,7 @@ exports.trade = (options) => {
     const bullShouldMoveStoploss = options.type === 'bull' && calcStoploss(price) > stoploss
     if (bearShouldMoveStoploss || bullShouldMoveStoploss) {
       stoploss = calcStoploss(price)
-      return `* ${options.type} ${dp2(percent)}% moving stop loss to: ${dp2(stoploss)}`
+      return `${time} ${options.type} ${dp2(percent)}% moving stop loss to: ${dp2(stoploss)}`
     }
 
     const bearComplete = options.type === 'bear' && price >= stoploss
@@ -32,7 +32,7 @@ exports.trade = (options) => {
       exit_time = time
       state = done
       const profit = options.type === 'bull' ? exit - entry : -(exit - entry)
-      return `* ${options.type} ${dp2(percent)}% trade complete: ${dp2(entry)}->${dp2(exit)} profit ${dp2(100*profit/entry)}% ${entry_time}-${exit_time}`
+      return `${time} ${options.type} ${dp2(percent)}% trade complete: ${dp2(entry)}->${dp2(exit)} profit ${dp2(100*profit/entry)}% ${entry_time}-${exit_time}`
     }
   }
 
@@ -40,7 +40,7 @@ exports.trade = (options) => {
   let state = initial
 
   const newTrade = (price, time) => {
-    return state(price, time)
+    return state(price, time.substring(11, 19))
   }
   newTrade.done = () => state === done
   return newTrade
