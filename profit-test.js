9f8fda7e811efec27a5e850b7d66d4b02ba570c3
sdclibbery <sdclibbery@googlemail.com>
2018-04-01 23:52:27 +0100

    test harness for stoploss tracker bot

--- profit-test.js
+++ profit-test.js
@@ -1,24 +1,43 @@
 const logger = {
-  info: () => {},
-  warn: () => {},
+  info: console.log,
+  warn: console.log,
   sync: {
-    info: () => {},
-    warn: () => {},
+    info: console.log,
+    warn: console.log,
   }
 }
 
 const options = { product: 'BTC-EUR', amount: 100, stoploss: 1 }
 
+let price = 1
+const nextPrice = () => {
+  price *= Math.pow((0.95 + 0.1*Math.random()), 0.1)
+  console.log('new price: ', price)
+  return price
+}
+let stopLoss
 const exchange = {
   roundQuote: (x) => x,
   roundBase: (x) => x,
-  formatQuote: () => 'fmtq',
-  formatBase: () => 'fmtb',
-  latestPrice: async () => await 1,
-  stopLoss: async () => await 'stopLoss-id',
-  waitForPriceChange: async () => await { price: 1.1 },
-  orderStatus: async () => await { filled: true, filledAmountInQuoteCurrency: 100 },
-  cancelOrder: async () => await {},
+  formatQuote: (x) => x,
+  formatBase: (x) => x,
+  latestPrice: async () => await nextPrice(),
+  stopLoss: async (stoplossPrice) => {
+    stopLoss = stoplossPrice
+    return await 'stopLoss-id'
+  },
+  waitForPriceChange: async () => await { price: nextPrice() },
+  orderStatus: async () => {
+    if (price < stopLoss)  {
+      return await { filled: true, filledAmountInQuoteCurrency: (100*stopLoss - 100*0.0025/*fees*/) }
+    } else {
+      return await { filled: false }
+    }
+  },
+  cancelOrder: async () => {
+    stopLoss = null
+    return await undefined
+  },
 }
 
 const framework = {
