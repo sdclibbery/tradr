4821e043c9a29e101dac883b8a1dba4d94c826ca
sdclibbery <sdclibbery@googlemail.com>
2018-04-07 09:00:32 +0100

    track profit/loss for testing

--- bot-ma-tracker.js
+++ bot-ma-tracker.js
@@ -2,8 +2,8 @@ const framework = require('./framework');
 
 const { options, logger, exchange } = framework.initBot([
   { name: 'product', alias: 'p', type: String, defaultValue: 'BTC-EUR', description: 'GDAX product' },
-  { name: 'close', alias: 'c', type: Number, defaultValue: 10, description: 'number of minutes to average together for the close moving average' },
-  { name: 'far', alias: 'f', type: Number, defaultValue: 15, description: 'number of minutes to average together for the far moving average' },
+  { name: 'close', alias: 'c', type: Number, defaultValue: 10, description: 'number of minutes to average together for the close moving average (up to 300)' },
+  { name: 'far', alias: 'f', type: Number, defaultValue: 15, description: 'number of minutes to average together for the far moving average (up to 300)' },
 ])
 
 framework.runBot(async () => {
@@ -14,6 +14,7 @@ framework.runBot(async () => {
   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
   let lastDirection
+  let tempBalanceForTesting = 0
   while (true) {
     const candles = await exchange.candles()
     const closeMa = movingAverage(candles, options.close)
@@ -22,9 +23,17 @@ framework.runBot(async () => {
     logger.debug(`close ma: ${closeMa}  far ma: ${farMa} direction ${direction}`)
 
     if (lastDirection && direction !== lastDirection) {
-      logger.info(`Direction change to ${direction}! at price ${candlePrice(candles[0])}; close ma: ${closeMa}  far ma: ${farMa}`)
+      const price = candlePrice(candles[0])
+      logger.info(`Direction change to ${direction}! at price ${price}; close ma: ${closeMa}  far ma: ${farMa}`)
+      if (direction == 'Up') {
+        tempBalanceForTesting -= price
+        logger.info(`Buy! tempBalanceForTesting: ${tempBalanceForTesting}`)
+      } else {
+        tempBalanceForTesting += price
+        logger.info(`Buy! tempBalanceForTesting: ${tempBalanceForTesting}`)
+      }
     }
-    
+
     lastDirection = direction
     await sleep(60*1000)
   }
