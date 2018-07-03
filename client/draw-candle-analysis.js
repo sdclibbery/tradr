1c85197c5f60921d11935f51d5a7ba48c10911a3
sdclibbery <sdclibbery@googlemail.com>
2018-07-03 23:39:54 +0100

    add lower hull

--- client/draw-candle-analysis.js
+++ client/draw-candle-analysis.js
@@ -33,35 +33,39 @@ drawCandleAnalysis = (canvas, candles, granularity) => {
   ctx.shadowBlur = 0
 
   let highs = candles.map(c => { return { x:(c.time*1000), y:(c.high) } }).reverse()
-  highs = reduceAnd(highs, 4, () => {})
-  highs = reduceAnd(highs, 5, lines)
+  highs = reduceAnd(highs, 4, () => {}, (l,r) => l<r)
+  highs = reduceAnd(highs, 5, lines, (l,r) => l<r)
+
+  let lows = candles.map(c => { return { x:(c.time*1000), y:(c.low) } }).reverse()
+  lows = reduceAnd(lows, 4, () => {}, (l,r) => l>r)
+  lows = reduceAnd(lows, 5, lines, (l,r) => l>r)
 }
 
-const reduceAnd = (hull, times, action) => {
+const reduceAnd = (hull, times, action, predicate) => {
   for (let i=0; i<times; i++) {
-    hull = reduceToHull(hull)
+    hull = reduceToHull(hull, predicate)
     action(hull)
   }
   return hull
 }
 
-const reduceToHull = (partialHull) => {
+const reduceToHull = (partialHull, predicate) => {
   const betterHull = []
   partialHull.forEach((p, idx, h) => {
     const prev = h[idx-1]
     const next = h[idx+1]
-    if (!prev || !next || !isBelowLine(prev, next, p)) {
+    if (!prev || !next || !isOutside(prev, next, p, predicate)) {
       betterHull.push(p)
     }
   })
   return betterHull
 }
 
-const isBelowLine = (ls, le, p) => {
+const isOutside = (ls, le, p, predicate) => {
   const m = (le.y - ls.y) / (le.x - ls.x)
   const c = ls.y - m*ls.x
   const yAtP = m*p.x + c
-  return p.y < yAtP
+  return predicate(p.y, yAtP)
 }
 
 assertSame = (actual, expected) => {
@@ -73,15 +77,15 @@ assertSame = (actual, expected) => {
   }
 }
 
-assertSame(isBelowLine({x:0,y:1}, {x:2,y:1}, {x:1,y:2}), false)
-assertSame(isBelowLine({x:0,y:1}, {x:2,y:1}, {x:1,y:1}), false)
-assertSame(isBelowLine({x:0,y:1}, {x:2,y:1}, {x:1,y:0}), true)
-assertSame(isBelowLine({x:0,y:1}, {x:2,y:5}, {x:1,y:3.1}), false)
-assertSame(isBelowLine({x:0,y:1}, {x:2,y:5}, {x:1,y:2.9}), true)
+assertSame(isOutside({x:0,y:1}, {x:2,y:1}, {x:1,y:2}, (l,r)=>l<r), false)
+assertSame(isOutside({x:0,y:1}, {x:2,y:1}, {x:1,y:1}, (l,r)=>l<r), false)
+assertSame(isOutside({x:0,y:1}, {x:2,y:1}, {x:1,y:0}, (l,r)=>l<r), true)
+assertSame(isOutside({x:0,y:1}, {x:2,y:5}, {x:1,y:3.1}, (l,r)=>l<r), false)
+assertSame(isOutside({x:0,y:1}, {x:2,y:5}, {x:1,y:2.9}, (l,r)=>l<r), true)
 
-assertSame(reduceToHull([{x:0,y:1}, {x:1,y:1}]), [{x:0,y:1}, {x:1,y:1}])
-assertSame(reduceToHull([{x:0,y:1}, {x:1,y:2}, {x:2,y:1}]), [{x:0,y:1}, {x:1,y:2}, {x:2,y:1}])
-assertSame(reduceToHull([{x:0,y:1}, {x:1,y:0}, {x:2,y:1}]), [{x:0,y:1}, {x:2,y:1}])
-assertSame(reduceToHull([{x:0,y:1}, {x:1,y:1.1}, {x:2,y:2}]), [{x:0,y:1}, {x:2,y:2}])
+assertSame(reduceToHull([{x:0,y:1}, {x:1,y:1}], (l,r)=>l<r), [{x:0,y:1}, {x:1,y:1}])
+assertSame(reduceToHull([{x:0,y:1}, {x:1,y:2}, {x:2,y:1}], (l,r)=>l<r), [{x:0,y:1}, {x:1,y:2}, {x:2,y:1}])
+assertSame(reduceToHull([{x:0,y:1}, {x:1,y:0}, {x:2,y:1}], (l,r)=>l<r), [{x:0,y:1}, {x:2,y:1}])
+assertSame(reduceToHull([{x:0,y:1}, {x:1,y:1.1}, {x:2,y:2}], (l,r)=>l<r), [{x:0,y:1}, {x:2,y:2}])
 // ? better algo: start with ALL points in hull, and remove any points that are
 //    below the line formed by the points either side
