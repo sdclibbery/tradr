4745ccd0355efc95e553ccaaae472ca6ebab556b
sdclibbery <sdclibbery@googlemail.com>
2018-09-15 08:39:32 +0100

    fix backup script

--- backup-to-googledrive.sh
+++ backup-to-googledrive.sh
@@ -3,6 +3,5 @@
 ls -a tradr.sqlite
 gzip -c tradr.sqlite > tradr.sqlite.gz
 ls -a tradr.sqlite.gz
-./gdrive-rpi upload -p tradr --delete tradr.sqlite.gz
-ls -a tradr.sqlite.gzip
+./gdrive-rpi upload --delete -p tradr tradr.sqlite.gz
 ./gdrive-rpi list -q "name contains 'tradr.sqlite'"
