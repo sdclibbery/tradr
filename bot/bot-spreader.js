e6d5b35c23bf7e038a96ea420a3b1530c9864c3f
sdclibbery <sdclibbery@googlemail.com>
2019-05-27 09:31:31 +0100

    spreader bot trades

--- bot/bot-spreader.js
+++ bot/bot-spreader.js
@@ -1,13 +1,15 @@
 const assert = require('assert')
 const coinbasePro = require('coinbase-pro')
+const exchangeFactory = require('../coinbasepro-exchange')
 const commandLineArgs = require('command-line-args')
 const loggerFactory = require('../logger')
 let credentials
 try { credentials = require('../coinbasepro-account-credentials') } catch (e) {}
 
 const logger = loggerFactory.createLogger(`${process.argv[1]}.log`)
+logger.debug = () => {}
 optionDefinitions = [
-  { name: 'amount', alias: 'a', type: Number, defaultValue: 0.003, description: 'amount to bot with in base currency (eg BTC)' },
+  { name: 'amount', alias: 'a', type: Number, defaultValue: 0.002, description: 'amount to bot with in base currency (eg BTC)' },
   { name: 'product', alias: 'p', type: String, defaultValue: 'BTC-GBP', description: 'coinbasepro product' },
   { name: 'help', alias: 'h', type: Boolean, defaultValue: false, description: 'Show this help' },
 ]
@@ -33,6 +35,8 @@ const orderbookSync = new coinbasePro.OrderbookSync(
   'wss://ws-feed.pro.coinbase.com',
   credentials
 )
+exchangeFactory.ready().then(() => {
+const exchange = exchangeFactory.createExchange(options, logger, orderbookSync)
 const orderBook = orderbookSync.books[product]
 // Monkey patch to fix the desync issue (https://github.com/coinbase/coinbase-pro-node/issues/308)
 const oldOrderBookStateMethod = orderBook.state
@@ -45,16 +49,39 @@ orderBook.state = function (book) {
 }
 // End of monkey patch
 const spread = {ask:0, bid:0}
+const orders = {buy:{}, sell:{}}
 const recent = []
 const amountInBase = () => parseFloat(options.amount)
 const amountInQuote = () => recent[0]&&options.amount*recent[0].price
-const minSpreadToTrade = () => amountInQuote()*0.01
+const minSpreadToTrade = () => amountInQuote()*0.1
+const minProfit = () => amountInQuote()*0.01
 logger.info(`BOT: Spreader starting for ${product} with ${amountInBase()}${baseCurrency}`)
 orderbookSync.on('message', (m) => {
   if (m.type == 'match') {
-    recent.unshift({price:parseFloat(m.price), side:m.side})
-    if (recent.length > 5) { recent.pop() }
+    // Own orders filled?
+    if (orders.buy.id == m.maker_order_id) {
+      exchange.orderStatus(orders.buy.id).then(({filled, price}) => {
+        if (filled) {
+          orders.buy = {}
+          orders.sellLimit = price + minProfit() // Set limit on how far the other side can track to avoid making a loss
+          logger.info(`BOT: buy order filled at ${dp2(price)}; set sell limit to ${dp2(orders.sellLimit)}`)
+        }
+      }).catch(logger.error)
+    } else if (orders.sell.id == m.maker_order_id) {
+      exchange.orderStatus(orders.sell.id).then(({filled, price}) => {
+        if (filled) {
+          orders.sell = {}
+          orders.buyLimit = price - minProfit() // Set limit on how far the other side can track to avoid making a loss
+          logger.info(`BOT: sell order filled at ${dp2(price)}; set sell limit to ${dp2(orders.buyLimit)}`)
+        }
+      }).catch(logger.error)
+    } else {
+      // Remember recent trades
+      recent.unshift({price:parseFloat(m.price), side:m.side})
+      if (recent.length > 5) { recent.pop() }
+    }
   }
+  // Get spread
   if (!orderBook._asks) {return}
   const minAsk = orderBook._asks.min()
   const maxBid = orderBook._bids.max()
@@ -67,12 +94,47 @@ orderbookSync.on('message', (m) => {
   spread.bid = bid
   process.stdout.write(`${green}${dp2(spread.bid)}${reset} - ${red}${dp2(spread.ask)}${reset} (${dp2(spread.ask - spread.bid)}) \t`+
     `${fmtRecent(recent[0])} ${recent.map(r => r&&(r.side=='buy'?red+'▼':green+'▲')).join('')}${reset}  \r`)
-  // Requires price volatility (ie recent orders include both spread edges) and spread > 1% of trade amount
+  // Helpers for buying and selling
+  const buyPrice = () => spread.bid + exchange.quoteStep
+  const sellPrice = () => spread.ask - exchange.quoteStep
+  const buy = () => {
+    orders.buy = { price: buyPrice() }
+    exchange
+      .buy(amountInBase()/2, orders.buy.price, 'spreader bot', `buy with spread ${dp2(spread.bid)} - ${dp2(spread.ask)}`)
+      .then(({id}) => orders.buy.id = id)
+      .catch(logger.error)
+  }
+  const sell = () => {
+    orders.sell = { price: sellPrice() }
+    exchange
+      .sell(amountInBase()/2, orders.sell.price, 'spreader bot', `sell with spread ${dp2(spread.bid)} - ${dp2(spread.ask)}`)
+      .then(({id}) => orders.sell.id = id)
+      .catch(logger.error)
+  }
+  // Track any existing orders against spread edges
+  if (orders.buy.id && orders.buy.price != buyPrice() && buyPrice() <= orders.buyLimit) {
+    logger.info(`BOT: moving buy from ${dp2(orders.buy.price)} to ${dp2(buyPrice())}`)
+    exchange.cancelOrder(orders.buy.id).then(buy).catch(logger.error)
+    orders.buy.id = undefined
+  }
+  if (orders.sell.id && orders.sell.price != sellPrice() && sellPrice() >= orders.sellLimit) {
+    logger.info(`BOT: moving sell from ${dp2(orders.sell.price)} to ${dp2(sellPrice())}`)
+    exchange.cancelOrder(orders.sell.id).then(sell).catch(logger.error)
+    orders.sell.id = undefined
+  }
+  // Before placing new orders, require price volatility (ie recent orders include both spread edges) and spread > 1% of trade amount
   if (recent.length < 2) {return}
-  if (!recent.some(r => r&&r.side=='buy')) {return}
-  if (!recent.some(r => r&&r.side=='sell')) {return}
+  if (!recent.some(r => r && r.side=='buy')) {return}
+  if (!recent.some(r => r && r.side=='sell')) {return}
   if ((spread.ask - spread.bid) < minSpreadToTrade()) {return}
-  process.stdout.write('\n')
-  // Do something...
-  logger.info(`bottable: ${dp2(spread.ask - spread.bid)} ${dp2(recent[0].price)}`)
+  // Place new orders
+  if (!orders.buy.price && !orders.sell.price) {
+    orders.buyLimit = Infinity
+    orders.sellLimit = 0
+    logger.info(`BOT:  buy in at ${dp2(buyPrice())}`)
+    buy()
+    logger.info(`BOT:  sell in at ${dp2(sellPrice())}`)
+    sell()
+  }
+})
 })
